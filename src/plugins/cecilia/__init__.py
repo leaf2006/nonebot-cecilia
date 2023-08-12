6a44670263a2f987d0297efd33fbca8f6d4a7e41
leaf2006 <yeshuo20061031@outlook.com>
2023-08-12 15:19:31 +0800

    first commit

---
+++ src/plugins/cecilia/__init__.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+from nonebot.adapters.onebot.v11 import MessageSegment
+from nonebot.plugin import on_command
+import random
+
+# 本地发送信息，一般情况下不会用这个
+
+# local_picture = on_command("白圣女")
+
+# @local_picture.handle()
+# async def handle_local_picture():
+#     # 本地图片位置
+#     path = Path(__file__).parent / "cecilia"".png"
+#     # 构造图片消息段
+#     image = MessageSegment.image(path)
+#     # 发送图片
+#     await local_picture.finish(image)
+
+network_picture = on_command("白圣女",aliases={"我要看白圣女","cecilia"})
+
+@network_picture.handle()
+async def handle_network_picture():
+    # str = "https://gitee.com/leaf2006/cecilia-img/raw/main/"
+    # str2 = "1"
+    # 构造图片消息段
+    url = "https://cdn.jsdelivr.net/gh/leaf2006/cecilia-img/"
+    local = "http://172.24.28.97:1234/" #本地服务器，如果github端timeout，可以在本地部署，速度更快
+    # direct = "https://raw.githubusercontent.com/leaf2006/cecilia-img/main/"
+    num = random.randint(1,21)
+    image = MessageSegment.image(local + str(num) + ".png")
+    # 发送图片
+    await network_picture.finish(image)
